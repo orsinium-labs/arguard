64b7ed85d5244ca81441a51d3074c17432552071
gram <git@orsinium.dev>
2023-08-30 11:37:03 +0200

    add smoke tests

--- arguard/analyzer_test.go
+++ arguard/analyzer_test.go
@@ -26,3 +26,33 @@ func TestAll(t *testing.T) {
 	testdata := filepath.Join(wd, "testdata")
 	analysistest.Run(t, testdata, aAnalyzer, "p")
 }
+
+// Run the linter on random stdlib packages and see if it explodes.
+func TestSmoke(t *testing.T) {
+	t.Parallel()
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Failed to get current directory: %v", err)
+	}
+	testdata := filepath.Join(wd, "testdata")
+
+	packages := []string{
+		"sync",
+		"flag",
+		// "os",
+		"fmt",
+		"go/ast",
+	}
+	for _, pkgName := range packages {
+		pkgName := pkgName
+		t.Run(pkgName, func(t *testing.T) {
+			t.Parallel()
+			cConfig := contracts.NewConfig()
+			cConfig.FollowImports = false
+			cAnalyzer := contracts.NewAnalyzer(cConfig)
+			aConfig := arguard.NewConfig()
+			aAnalyzer := arguard.NewAnalyzer(aConfig, &cAnalyzer)
+			analysistest.Run(t, testdata, aAnalyzer, pkgName)
+		})
+	}
+}
--- contracts/analyzer_test.go
+++ contracts/analyzer_test.go
@@ -34,3 +34,31 @@ func TestImports(t *testing.T) {
 	analyzer := contracts.NewAnalyzer(config)
 	analysistest.Run(t, testdata, &analyzer, "i")
 }
+
+// Run the linter on random stdlib packages and see if it explodes.
+func TestSmoke(t *testing.T) {
+	t.Parallel()
+	wd, err := os.Getwd()
+	if err != nil {
+		t.Fatalf("Failed to get current directory: %v", err)
+	}
+	testdata := filepath.Join(wd, "testdata")
+
+	packages := []string{
+		"sync",
+		"flag",
+		"os",
+		"fmt",
+		"go/ast",
+	}
+	for _, pkgName := range packages {
+		pkgName := pkgName
+		t.Run(pkgName, func(t *testing.T) {
+			t.Parallel()
+			config := contracts.NewConfig()
+			config.FollowImports = false
+			analyzer := contracts.NewAnalyzer(config)
+			analysistest.Run(t, testdata, &analyzer, pkgName)
+		})
+	}
+}
